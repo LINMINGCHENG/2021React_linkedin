bed5bb34323b3103ea1c8d1d198c68b5f2451a34
mingcheng lin <you@example.com>
2021-06-03 23:47:28 -0400

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Header.css"
+function Header() {
+    return (
+        <div className="headers">
+            This IS HEADER.
+           <div className="header_left">
+
+           </div>
+
+           <div className="header_right">
+           
+           </div>
+        </div>
+    )
+}
+
+export default Header
